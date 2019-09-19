aca3771b576cee9e3d0a744d6dc7776ede552995
Ricardo Barcellos <ricardoafb3000@gmail.com>
2019-09-18 23:07:54 -0300

    razzle config

---
+++ src/client.js
@@ -0,0 +1,15 @@
+import App from './App';
+import { BrowserRouter } from 'react-router-dom';
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById('root'));
+
+if (module.hot) {
+  module.hot.accept();
+}
